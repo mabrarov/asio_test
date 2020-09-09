ddcc8c425e617ddba1d4cb7b11bb37c647dc59af
Marat Abrarov <abrarov@gmail.com>
2020-09-10 01:10:29 +0300

    Minimal application using Boost.Asio

---
+++ static_c_runtime_overrides.cmake
@@ -0,0 +1,27 @@
+# 
+# Copyright (c) 2010-2015 Marat Abrarov (abrarov@gmail.com)
+#
+# Distributed under the Boost Software License, Version 1.0. (See accompanying
+# file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+#
+
+cmake_minimum_required(VERSION 2.8.11)
+
+# Use CMAKE_USER_MAKE_RULES_OVERRIDE command line argument to point these rules
+
+if(MSVC)
+    set(CMAKE_C_FLAGS_DEBUG_INIT          "/D_DEBUG /MTd /Zi /Ob0 /Od /RTC1")
+    set(CMAKE_C_FLAGS_MINSIZEREL_INIT     "/MT /O1 /Ob1 /D NDEBUG")
+    set(CMAKE_C_FLAGS_RELEASE_INIT        "/MT /O2 /Ob2 /D NDEBUG")
+    set(CMAKE_C_FLAGS_RELWITHDEBINFO_INIT "/MT /Zi /O2 /Ob1 /D NDEBUG")
+endif()
+
+if(CMAKE_COMPILER_IS_GNUCC)
+    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -static -static-libgcc")
+endif()
+
+set(ICU_USE_STATIC_LIBS ON)
+
+if(MSVC)
+    set(GTEST_MSVC_SEARCH "MT")
+endif()
---
+++ static_cxx_runtime_overrides.cmake
@@ -0,0 +1,30 @@
+#
+# Copyright (c) 2010-2015 Marat Abrarov (abrarov@gmail.com)
+#
+# Distributed under the Boost Software License, Version 1.0. (See accompanying
+# file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+#
+
+cmake_minimum_required(VERSION 2.8.11)
+
+# Use CMAKE_USER_MAKE_RULES_OVERRIDE_CXX command line argument to point these rules
+
+if(MSVC)
+    set(CMAKE_CXX_FLAGS_DEBUG_INIT          "/D_DEBUG /MTd /Zi /Ob0 /Od /RTC1")
+    set(CMAKE_CXX_FLAGS_MINSIZEREL_INIT     "/MT /O1 /Ob1 /D NDEBUG")
+    set(CMAKE_CXX_FLAGS_RELEASE_INIT        "/MT /O2 /Ob2 /D NDEBUG")
+    set(CMAKE_CXX_FLAGS_RELWITHDEBINFO_INIT "/MT /Zi /O2 /Ob1 /D NDEBUG")
+endif()
+
+if(CMAKE_COMPILER_IS_GNUCC)
+    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -static -static-libgcc -static-libstdc++")
+endif()
+
+set(Boost_USE_STATIC_LIBS    ON)
+set(Boost_USE_STATIC_RUNTIME ON)
+
+set(ICU_USE_STATIC_LIBS      ON)
+
+if(MSVC)
+    set(GTEST_MSVC_SEARCH    "MT")
+endif()
