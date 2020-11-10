ae5f4ca93b7a9a91094737cf9cd55be6c9d7310c
Marat Abrarov <abrarov@gmail.com>
2020-11-10 15:22:14 +0300

    travis-ci.org -> travis-ci.com migration

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Tests for Boost.Asio
 
-[![Travis CI build status](https://travis-ci.org/mabrarov/asio_test.svg?branch=master)](https://travis-ci.org/mabrarov/asio_test) [![AppVeyor CI build status](https://ci.appveyor.com/api/projects/status/7avx0sy3ec3d4eq5/branch/master?svg=true)](https://ci.appveyor.com/project/mabrarov/asio-test)  
+[![Travis CI build status](https://travis-ci.com/mabrarov/asio_test.svg?branch=master)](https://travis-ci.com/mabrarov/asio_test) [![AppVeyor CI build status](https://ci.appveyor.com/api/projects/status/7avx0sy3ec3d4eq5/branch/master?svg=true)](https://ci.appveyor.com/project/mabrarov/asio-test)  
 
 ## Building
 
--- scripts/travis/build.sh
+++ scripts/travis/build.sh
@@ -7,22 +7,22 @@ rm -rf "${BUILD_HOME}"
 mkdir -p "${BUILD_HOME}"
 cd "${BUILD_HOME}"
 
-cmake_gen_cmd="cmake -D CMAKE_C_COMPILER=\"${C_COMPILER}\" -D CMAKE_CXX_COMPILER=\"${CXX_COMPILER}\" -D CMAKE_BUILD_TYPE=\"${BUILD_TYPE}\""
+cmake_gen_cmd="cmake -D CMAKE_C_COMPILER=$(printf "%q" "${C_COMPILER}") -D CMAKE_CXX_COMPILER=$(printf "%q" "${CXX_COMPILER}") -D CMAKE_BUILD_TYPE=$(printf "%q" "${BUILD_TYPE}")"
 if [[ -n "${BOOST_HOME+x}" ]]; then
   echo "Building with Boost ${BOOST_VERSION} located at ${BOOST_HOME}"
-  cmake_gen_cmd="${cmake_gen_cmd} -D CMAKE_SKIP_BUILD_RPATH=ON -D Boost_NO_SYSTEM_PATHS=ON -D BOOST_INCLUDEDIR=\"${BOOST_HOME}/include\" -D BOOST_LIBRARYDIR=\"${BOOST_HOME}/lib\""
+  cmake_gen_cmd="${cmake_gen_cmd} -D CMAKE_SKIP_BUILD_RPATH=ON -D Boost_NO_SYSTEM_PATHS=ON -D BOOST_INCLUDEDIR=$(printf "%q" "${BOOST_HOME}/include") -D BOOST_LIBRARYDIR=$(printf "%q" "${BOOST_HOME}/lib")"
 else
   echo "Building with Boost ${BOOST_VERSION} located at system paths"
 fi
-cmake_gen_cmd="${cmake_gen_cmd} \"${TRAVIS_BUILD_DIR}\""
+cmake_gen_cmd="${cmake_gen_cmd} $(printf "%q" "${TRAVIS_BUILD_DIR}")"
 echo "CMake project generation command: ${cmake_gen_cmd}"
 eval "${cmake_gen_cmd}"
 
-build_cmd="cmake --build \"${BUILD_HOME}\" --config \"${BUILD_TYPE}\""
+build_cmd="cmake --build $(printf "%q" "${BUILD_HOME}") --config $(printf "%q" "${BUILD_TYPE}")"
 echo "CMake project build command: ${build_cmd}"
 eval "${build_cmd}"
 
-test_cmd="\"${BUILD_HOME}/src/strand_test/strand_test\" --use-strand-wrap \"${STRAND_TEST_USE_STRAND_WRAP}\" --threads \"${STRAND_TEST_THREAD_NUM}\" --duration \"${STRAND_TEST_HANDLER_DURATION}\" --init \"${STRAND_TEST_INIT_HANDLER_NUM}\" --concurrent \"${STRAND_TEST_CONCURRENT_HANDLER_NUM}\" --strand \"${STRAND_TEST_STRAND_HANDLER_NUM}\""
+test_cmd="$(printf "%q" "${BUILD_HOME}/src/strand_test/strand_test") --use-strand-wrap $(printf "%q" "${STRAND_TEST_USE_STRAND_WRAP}") --threads $(printf "%q" "${STRAND_TEST_THREAD_NUM}") --duration $(printf "%q" "${STRAND_TEST_HANDLER_DURATION}") --init $(printf "%q" "${STRAND_TEST_INIT_HANDLER_NUM}") --concurrent $(printf "%q" "${STRAND_TEST_CONCURRENT_HANDLER_NUM}") --strand $(printf "%q" "${STRAND_TEST_STRAND_HANDLER_NUM}")"
 echo "Running test: ${test_cmd}"
 if eval "${test_cmd}" ; then
   echo "Test succeeded"
